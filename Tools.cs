dd06f6babdda6b0541efe2a439f0be79c4bb3609
FrostyCave <95409965+FrostyCave@users.noreply.github.com>
2022-04-20 10:55:42 +0200

    .

--- Tools.cs
+++ Tools.cs
@@ -5,19 +5,19 @@
         public static int counter;
         public static void PrintArray(string[] input)
         {
-            //foreach (var item in input) 
-            //{
-            //    Console.WriteLine(item);
-            //}
+            foreach (var item in input) 
+            {
+                Console.WriteLine(item);
+            }
             for (int i = 0; i < input.Length; i++)
             {
                 Console.WriteLine(input[i] + " ");
             }
-            //while (counter < input.Length)
-            //{
-            //    Console.WriteLine(input[counter]);
-            //    counter++;
-            //}
+            while (counter < input.Length)
+            {
+                Console.WriteLine(input[counter]);
+                counter++;
+            }
 
         }
 
